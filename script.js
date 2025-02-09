08e8c206f449bcaf6debb03f18a8da6c7d509c37
chrisdomfeh <chrisasumadu9@gmail.com>
2025-02-09 22:16:13 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let valueSearch= document.getElementById("valueSearch");
+let city= document.getElementById("city");
+let temperature = document.getElementById("temperature");
+let description = document.querySelector(".description");
+let clouds = document.getElementById("clouds");
+let humidity = document.getElementById("humidity");
+let pressure = document.getElementById("pressure");
+let main = document.querySelector("main");
+let form = document.querySelector("form");
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    if(valueSearch.value !==""){
+        searchWeather();
+    }
+})
+let id = "10a20245bf7129b49c3fe1823cce8a64";
+let url = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid="+id;
+const searchWeather=()=>{
+    fetch(url + '&q=' + valueSearch.value)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.cod == 200){
+            city.querySelector("figcaption").innerHTML=data.name;
+            city.querySelector("img").src="https://flagsapi.com/"+data.sys.country+"/shiny/32.png";
+            temperature.querySelector("img").src="https://openweathermap.org/img/wn/"+data.weather[0].icon+"@4x.png";
+            temperature.querySelector("figcaption span").innerHTML=data.main.temp;
+            description.innerText = data.weather[0].description;
+            humidity.innerText =data.main.humidity;
+            pressure.innerText = data.main.pressure;
+        }else{
+            main.classList.add("error");
+            setTimeout(()=>{
+                main.classList.remove("error");
+            },1000)
+        }
+
+        valueSearch.value = "";
+    })
+}
+const initApp = () => {
+    valueSearch.value = "Washington";
+    searchWeather();
+}
+initApp();
